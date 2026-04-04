6f3d67aec496bf19ee511777f25e450f43e6d646
arif-kiz <arif.muhammed.kiz@gmail.com>
2026-04-04 18:45:30 +0530

    Added spliting in middle and interchages them.

--- card_shuffling/src/main.rs
+++ card_shuffling/src/main.rs
@@ -1,3 +1,14 @@
+// Splits the deck into two halves and interchanges them
+fn middle_split(cards: &mut Vec<i32>) {
+    let mut first_half: Vec<i32> = cards[0..168/2].iter().copied().collect();
+    let mut second_half: Vec<i32> = cards[168/2..168].iter().copied().collect();
+    second_half.append(&mut first_half);
+    *cards = second_half;
+}
 fn main() {
-    println!("Hello World");
+    let mut cards: Vec<_> = (1..169).into_iter().collect();
+    println!("{:?}", cards);
+
+    middle_split(&mut cards);
+    println!("{:?}", cards);
 }
