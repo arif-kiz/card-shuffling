3f7fb26454ad9dd51c4a6c119345077a4efd721d
arif-kiz <arif.muhammed.kiz@gmail.com>
2026-04-06 14:30:20 +0530

    optimized middle_split using rotate_left and added split_at functionality

--- card_shuffling/src/main.rs
+++ card_shuffling/src/main.rs
@@ -1,14 +1,23 @@
 // Splits the deck into two halves and interchanges them
 fn middle_split(cards: &mut Vec<i32>) {
-    let mut first_half: Vec<i32> = cards[0..168/2].iter().copied().collect();
-    let mut second_half: Vec<i32> = cards[168/2..168].iter().copied().collect();
-    second_half.append(&mut first_half);
-    *cards = second_half;
+    let mid = cards.len() / 2;
+    cards.rotate_left(mid);
 }
+
+// Splits the deck into two part at a point and interchanges them
+fn split_at(cards: &mut Vec<i32>, i: usize) {
+    if i <= cards.len() {
+        cards.rotate_left(i);
+    }
+}
+
 fn main() {
     let mut cards: Vec<_> = (1..169).into_iter().collect();
-    println!("{:?}", cards);
+    println!("Fisrt: {:?}", &cards[0..10]);
 
     middle_split(&mut cards);
-    println!("{:?}", cards);
+    println!("Middle Split: {:?}", &cards[..10]);
+
+    split_at(&mut cards, 50);
+    println!("Split at 50: {:?}", &cards[..10]);
 }
