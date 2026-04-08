7708f58b8817e5f3b72a66222c87563c9d233b6d
arif-kiz <arif.muhammed.kiz@gmail.com>
2026-04-08 16:27:01 +0530

    feat: implement riffle shuffle and take_from_middle functions while updating card type to usize

--- card_shuffling/src/main.rs
+++ card_shuffling/src/main.rs
@@ -1,18 +1,45 @@
+const NUMBER_OF_CARDS: usize = 168;
+
 // Splits the deck into two halves and interchanges them
-fn middle_split(cards: &mut Vec<i32>) {
+fn middle_split(cards: &mut Vec<usize>) {
     let mid = cards.len() / 2;
     cards.rotate_left(mid);
 }
 
 // Splits the deck into two part at a point and interchanges them
-fn split_at(cards: &mut Vec<i32>, i: usize) {
+fn split_at(cards: &mut Vec<usize>, i: usize) {
     if i <= cards.len() {
         cards.rotate_left(i);
     }
 }
 
+// Riffle shuffle
+fn riffle_shuffle(cards: &mut Vec<usize>) {
+    let mid = cards.len() / 2;
+    let mut left = cards[..mid].to_vec();
+    left.reverse();
+    let mut right = cards[mid..].to_vec();
+    right.reverse();
+    
+    for i in 0..cards.len() {
+        if i % 2 == 0 {
+            cards[i] = left.pop().unwrap();
+        } else {
+            cards[i] = right.pop().unwrap();
+        }
+    }
+}
+
+// Take from middle
+fn take_from_middle(cards: &mut Vec<usize>, i: usize, j: usize) {
+    if j <= i || j > cards.len() {
+        return
+    }
+    cards[0..j].rotate_right(j-i);
+}
+
 fn main() {
-    let mut cards: Vec<_> = (1..169).into_iter().collect();
+    let mut cards: Vec<_> = (0..NUMBER_OF_CARDS).into_iter().collect();
     println!("Fisrt: {:?}", &cards[0..10]);
 
     middle_split(&mut cards);
@@ -20,4 +47,10 @@ fn main() {
 
     split_at(&mut cards, 50);
     println!("Split at 50: {:?}", &cards[..10]);
+
+    riffle_shuffle(&mut cards);
+    println!("Riffle Shuffle: {:?}", &cards[..10]);
+
+    take_from_middle(&mut cards, 20, 80);
+    println!("From middle: {:?}", &cards[..10]);
 }
